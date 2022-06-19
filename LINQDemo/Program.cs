5455623762212725011f2df0cd4ca312c4f36cc7
Amoriss <amoriss@protonmail.com>
2022-06-18 22:06:49 -0500

    initial commit

---
+++ LINQDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using LinqLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Examples of collections: 
+            // 1) lists
+            // 2) arrays
+
+
+            //collection 1
+            List<Person> people = ListManager.LoadSimpleData();
+            people = people.OrderBy(x => x.LastName).ToList();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($" {person.FirstName} {person.LastName} : Years of Experience {person.YearsOfExperience}");
+            }
+
+            //collection 2
+            var array = new int[] {1, 24, 29, 30, 42, 302};
+
+            //foreach loop
+
+            //using LINQ
+
+        }
+    }
+}
---
+++ LinqLibrary/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinqLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearsOfExperience { get; set; }
+
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName}  {LastName}";
+            }
+        }
+    }
+}
